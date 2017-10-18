8a00c2f7346b18184e7f6d2e6d50f727642b2b73
JongHo <jhleed@naver.com>
2017-10-18 20:51:11 +0900

    Reactive Streams : Publishing and Consuming Messages : 공식 문서를 참고한 주석 추가

--- src/reactive_streams/EndSubscriber.java
+++ src/reactive_streams/EndSubscriber.java
@@ -8,12 +8,21 @@ public class EndSubscriber<T> implements Flow.Subscriber<T> {
     private Flow.Subscription subscription;
     public List<T> consumedElements = new LinkedList<>();
 
+    /**
+     * 지정된 Subscription에 대해 다른 Subscriber 메서드를 호출하기 전에 호출되는 메서드입니다.
+     *
+     * @param subscription
+     */
     @Override
     public void onSubscribe(Flow.Subscription subscription) {
         this.subscription = subscription;
         subscription.request(1);
     }
 
+    /**
+     * Subscription의 다음 항목으로 호출되는 메서드입니다.
+     * @param item
+     */
     @Override
     public void onNext(T item) {
         System.out.println("Got : " + item);
@@ -21,11 +30,21 @@ public class EndSubscriber<T> implements Flow.Subscriber<T> {
         subscription.request(1);
     }
 
+    /**
+     * Publisher 또는 Subscription에서 발생하는 복구 할 수없는 오류로 인해 호출 된 메서드입니다. 그 후에는 Subscription으로 다른 Subscriber 메서드가 호출되지 않습니다.
+     *
+     * Publisher가 Subscriber에게 항목을 발행하지 못하게하는 오류가 발생하면 해당 Subscriber는 onError를 수신 한 다음 더 이상 메시지를받지 않습니다.
+     * @param t
+     */
     @Override
     public void onError(Throwable t) {
         t.printStackTrace();
     }
 
+    /**
+     * 아직 오류로 종료되지 않은 Subscription에 대해 추가 Subscriber 메서드 호출이 발생하지 않는 것으로 알려진 후 Subscription에서 다른 Subscriber 메서드를 호출하지 않는 경우 호출되는 메서드입니다.
+     * 더 이상의 메시지가 발행되지 않을 것으로 알려진 경우, Subscriber는 onComplete를 실행합니다.
+     */
     @Override
     public void onComplete() {
         System.out.println("Done");
--- test/reactive_streams/EndSubscriberTest.java
+++ test/reactive_streams/EndSubscriberTest.java
@@ -12,22 +12,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class EndSubscriberTest {
     @Test
     public void whenSubscribeToIt_thenShouldConsumeAll() throws InterruptedException {
-        // given
+        // given - 게시자, 구독자 설정
         SubmissionPublisher<String> publisher = new SubmissionPublisher<>();
         EndSubscriber<String> subscriber = new EndSubscriber<>();
         publisher.subscribe(subscriber);
         List<String> items = List.of("1", "x", "2", "x", "3", "x");
 
-        // when
+        // when 게시자가 구독자에게 데이터 스트림 전송 (submit)
         assertThat(publisher.getNumberOfSubscribers()).isEqualTo(1);
-        items.forEach(publisher::submit);
+        items.forEach(publisher::submit); //구독자에게 처리를 맡기고 기다린다!
         publisher.close();
 
-        // then
-        await().atMost(1000, TimeUnit.MILLISECONDS).until(() -> {
-            //subscriber의 consumedElements에 값이 세팅되지 않음.
-            assertThat(subscriber.consumedElements).containsExactlyElementsOf(items);
-        });
+        // then - (대기 시간을 1초로 제한)
+        // 구독자의 onNext 메서드가 실행된 것을 확인한다.
+        await().atMost(1000, TimeUnit.MILLISECONDS).until(() -> assertThat(subscriber.consumedElements).containsExactlyElementsOf(items));
     }
 
 }
